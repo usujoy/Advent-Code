09b0b668ef37ec9989678275955dcc7619a2b226
usujoy <183721610+usujoy@users.noreply.github.com>
2025-02-03 11:49:11 +0530

    changed the approach of calculating mass

--- 2019/day_01/day_01.js
+++ 2019/day_01/day_01.js
@@ -1,25 +1,24 @@
-const masses = Deno.readTextFileSync("./day-1-input.txt");
+const totalFuel = (shipMasses) => {
+  const eachMasses = shipMasses.map((mass) => {
+    const newMass = Math.floor(mass / 3) - 2;
 
-const massesInNumbers = masses.split("\n").map((number) => +number);
+    return newMass < 0 ? 0 : newMass;
+  });
 
-const fuelNeeded = (mass) => {
-  return Math.floor(mass / 3) - 2;
-};
-
-const totalFuel = (fuel) => {
-  const requiredFuel = fuelNeeded(fuel);
-  if (requiredFuel < 2) {
-    return 0;
+  if (eachMasses.every((mass) => mass === 0)) {
+    return eachMasses;
   }
 
-  return requiredFuel + totalFuel(requiredFuel);
+  return totalFuel(eachMasses).map((mass, i) => mass + eachMasses[i]);
 };
 
-const fuel = massesInNumbers.reduce(
-  (sum, masses) => sum + fuelNeeded(masses),
-  0
-);
+const add = (value1, value2) => value1 + value2;
 
-const totalFuelNeeded = fuel + totalFuel(fuel);
+const main = () => {
+  const data = Deno.readTextFileSync("./2019/day_01/day-1-input.txt");
+  const masses = data.split("\n").map((mass) => +mass);
 
-console.log(totalFuelNeeded);
+  return totalFuel(masses).reduce(add, 0);
+};
+
+console.log(main());
